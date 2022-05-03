cffd82d56df09feeb0dbf78f8753288533a2383b
ksenideni <denishchenkovakseni@mail.ru>
2022-05-03 12:18:29 +0300

    Добавлена документация

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 from uuid import uuid4
 from aiogram import Bot, Dispatcher, executor, types
 
+# полная документация в формате .html
+# python -m pydoc -w  main
+
 API_TOKEN = os.getenv('telegram_api_token_wordle')
-
 GAME_NAME = 'wordle'
-
 GAME_URL = 'http://51.250.17.140/'
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -25,11 +26,15 @@ keyboard.add(play_btn, help_btn, rules_btn)
 
 @dp.message_handler(commands='start')
 async def send_welcome(message: types.Message):
+    """Устанавливает клавиатуру, в которой  первая кнопка должна запускать игру."""
+
     await bot.send_game(message.chat.id, game_short_name=GAME_NAME, reply_markup=keyboard)
 
 
 @dp.inline_handler()
 async def send_game(inline_query: types.InlineQuery):
+    """Отправляет ответ на inline query, чтобы игра высвечивалась при вводе  @wordle_rtu_bot."""
+
     await bot.answer_inline_query(inline_query.id,
                                   [types.InlineQueryResultGame(id=str(uuid4()),
                                                                game_short_name=GAME_NAME)])
@@ -37,6 +42,8 @@ async def send_game(inline_query: types.InlineQuery):
 
 @dp.callback_query_handler(lambda message: message.game_short_name == GAME_NAME)
 async def process_callback_button1(callback_query: types.CallbackQuery):
+    """Перенаправляет юзера на его игру."""
+
     # print(callback_query.game_short_name)
     print(callback_query)
     # print(callback_query.message)
@@ -47,11 +54,15 @@ async def process_callback_button1(callback_query: types.CallbackQuery):
 
 @dp.callback_query_handler(lambda c: c.data == 'help')
 async def process_callback_button2(callback_query: types.CallbackQuery):
+    """Возвращает юзеру вспомогательную информацию об игре в качестве уведомления вверху экрана."""
+
     await bot.answer_callback_query(callback_query.id, 'Запустите по кнопке play')
 
 
 @dp.callback_query_handler(lambda c: c.data == 'rules')
 async def process_callback_button3(callback_query: types.CallbackQuery):
+    """Возвращает юзеру правила игры в качестве уведомления вверху экрана."""
+
     await bot.answer_callback_query(callback_query.id, 'Рейтинг будет считаться так-то так-то.')
 
 
